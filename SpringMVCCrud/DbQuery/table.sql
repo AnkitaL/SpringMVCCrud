7f5e0e800360c58a991cabadce319307b54eb183
AnkitaL <DHRUV SHELADIA@DESKTOP-F3OFT9D>
2021-03-04 10:47:33 +0530

    This is Full Crud Demo Code with Spring mvc Framework.

---
+++ SpringMVCCrud/DbQuery/table.sql
@@ -0,0 +1,5 @@
+insert into country value(1,'INDIA');
+insert into country value(2,'SRILANKA');
+insert into country value(3,'US');
+insert into country value(4,'CANADA');
+insert into country value(5,'RUSSIA');
